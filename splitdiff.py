c44b46d1bd6169d0946caeb4205d100f1ec04338
Michael Buesch <mb@bu3sch.de>
2010-07-30 23:46:21 +0200

    add splitdiff.py
    Signed-off-by: Michael Buesch <mb@bu3sch.de>

---
+++ splitdiff.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+
+
+prefix = 1
+inHeader = True
+lineNr = 0
+fileNr = 0
+fd = None
+
+def die(msg=None):
+	if msg:
+		sys.stderr.write(msg + "\n")
+	sys.exit(1)
+
+def out(data):
+	fd.write(data)
+
+try:
+	(opts, args) = getopt.getopt(sys.argv[1:], "p:")
+except getopt.GetoptError:
+	usage()
+	die()
+for (o, v) in opts:
+	if o in ("-p",):
+		try:
+			prefix = int(v)
+			if prefix < 0:
+				raise ValueError
+		except ValueError:
+			die("Invalid -p")
+
+while 1:
+	line = sys.stdin.readline()
+	if not line:
+		break
+	lineNr += 1
+	if line.startswith("diff "):
+		path = line.strip().split(" ")[-1]
+		path = path.split("/")
+		if len(path) <= prefix:
+			die("Could not strip all prefix")
+		path = path[prefix:]
+		filename = "-".join(path)
+		filename = "%03d-%s.diff" % (fileNr, filename)
+		fileNr += 1
+		fd = file(filename, "w")
+		out(line)
+		inHeader = False
+		continue
+	if line.startswith("--- ") or \
+	   line.startswith("+++ ") or \
+	   line.startswith("@@ ") or \
+	   line.startswith("+") or \
+	   line.startswith("-") or \
+	   line.startswith(" "):
+		if not inHeader:
+			out(line)
+		continue
+
+	if inHeader:
+		continue
+	die("Parse error in line %d" % lineNr)
+
+sys.exit(0)
