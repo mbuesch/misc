f4bc19bbe638a0af59145cbbc1045714b3c08dd3
Michael Buesch <m@bues.ch>
2011-10-12 18:32:45 +0200

    Add stdio buffer hack
    Signed-off-by: Michael Buesch <m@bues.ch>

---
+++ stdiobufhack/stdiobufhack.c
@@ -0,0 +1,34 @@
+/* Simple LD_PRELOAD library to force stdout/stderr into line-buffered mode.
+ * This file is released to the Public Domain. */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+
+#define TARGET_MODE	_IOLBF
+
+
+static int (*libc_setvbuf)(FILE *stream, char *buf, int mode, size_t size);
+
+int setvbuf(FILE *stream, char *buf, int mode, size_t size)
+{
+	if (!libc_setvbuf)
+		libc_setvbuf = dlsym(RTLD_NEXT, "setvbuf");
+	if (!libc_setvbuf) {
+		fprintf(stderr, "stdiobufhack: Did not find libc setvbuf()\n");
+		exit(1);
+	}
+
+	if (stream == stdout || stream == stderr)
+		mode = TARGET_MODE;
+
+	return libc_setvbuf(stream, buf, mode, size);
+}
+
+static void __attribute__((__constructor__)) stdiobufhack_ctor(void)
+{
+	setvbuf(stdout, NULL, TARGET_MODE, 1024);
+	setvbuf(stderr, NULL, TARGET_MODE, 1024);
+}
