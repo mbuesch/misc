48d40226edf3be60e9c0e41a8289f77ae3b50720
Michael Buesch <m@bues.ch>
2011-10-12 16:31:56 +0200

    sauerstats: Move sauerbraten regexes to sauerbraten parser
    Signed-off-by: Michael Buesch <m@bues.ch>

--- sauerbraten/sauerstats.py
+++ sauerbraten/sauerstats.py
@@ -15,60 +15,8 @@ import pygraphviz as gv
 
 debug = False
 
-
 re_iso_timestamp = re.compile(r'(\d\d\d\d)-(\d\d)-(\d\d)T(\d\d):(\d\d):(\d\d)\.(\d\d\d\d\d\d)')
 ISO_TIMESTAMP_LEN = 26
-_re_name = r'(.*)'
-
-re_init = re.compile(r'^init: .*$')
-re_waypoints = re.compile(r'^loaded (\d+) waypoints from ' + _re_name + r'$')
-re_intermission = re.compile(r'^intermission:$')
-re_info = re.compile(r'^Info: .*$')
-re_rendering = re.compile(r'^Rendering using .*$')
-
-re_game_mode = re.compile(r'^game mode is ' + _re_name + r'$')
-re_game_ended = re.compile(r'^game has ended!$')
-
-re_you_disconn = re.compile(r'^disconnected$')
-re_player_disconn = re.compile(r'^player ' + _re_name + r' disconnected$')
-re_conn_attempt = re.compile(r'^attempting to connect.*$')
-re_connected = re.compile(r'^connected to server$')
-re_player_connected = re.compile(r'^connected: ' + _re_name + r'$')
-re_connected_from = re.compile(r'^' + _re_name + r' connected from ' + _re_name + r'$')
-re_connected_from2 = re.compile(r'^' + _re_name + r' is fragging in ' + _re_name + r'$')
-re_disconn_attempt = re.compile(r'^attempting to disconnect\.\.\.$')
-re_rename = re.compile(r'^' + _re_name + r' is now known as ' + _re_name + r'$')
-
-re_read_map = re.compile(r'^read map (.*) \(\d+\.\d+ seconds\)$')
-re_suggest = re.compile(r'^' + _re_name + r' suggests ' + _re_name + r' on map ' +
-			_re_name + r' \(select map to vote\)$')
-
-re_you_got_fragged = re.compile(r'^you got fragged by ' + _re_name + r'$')
-re_fragged = re.compile(r'^' + _re_name + r' fragged ' + _re_name + r'$')
-re_fragged_teammate = re.compile(r'^' + _re_name + r' fragged a teammate \(' + _re_name + r'\)$')
-re_yougotkilledby_teammate = re.compile(r'^you got fragged by a teammate \(' + _re_name + r'\)$')
-re_suicide = re.compile(r'^' + _re_name + r' suicided!?$')
-
-re_dropped_your = re.compile(r'^' + _re_name + r' dropped your flag$')
-re_dropped_enemy = re.compile(r'^' + _re_name + r' dropped the enemy flag$')
-re_stole_your = re.compile(r'^' + _re_name + r' stole your flag$')
-re_stole_enemy = re.compile(r'^' + _re_name + r' stole the enemy flag$')
-re_picked_your = re.compile(r'^' + _re_name + r' picked up your flag$')
-re_picked_enemy = re.compile(r'^' + _re_name + r' picked up the enemy flag$')
-re_score_your = re.compile(r'^' + _re_name + r' scored for your team$')
-re_score_enemy = re.compile(r'^' + _re_name + r' scored for the enemy team$')
-re_returned_your = re.compile(r'^' + _re_name + r' returned your flag$')
-re_returned_enemy = re.compile(r'^' + _re_name + r' returned the enemy flag$')
-re_stopped_by = re.compile(r'^' + _re_name + r' was stopped by ' + _re_name + r'$')
-
-re_rampage = re.compile(r'^' + _re_name + r' is on a RAMPAGE!!$')
-re_dominate = re.compile(r'^' + _re_name + r' is DOMINATING!!$')
-re_triplekill = re.compile(r'^' + _re_name + r' scored a TRIPLE KILL!$')
-
-re_msg = re.compile(r'^' + _re_name + r': .*$')
-# chat annotation patch
-re_chat = re.compile(r'^chat-message: ' + _re_name + r':\s*(.*)$')
-re_teamchat = re.compile(r'^teamchat-message: ' + _re_name + r':\s*(.*)$')
 
 
 def debugMsg(msg):
@@ -353,7 +301,63 @@ class Game(object):
 			ret.append("")
 		return "\n".join(ret)
 
-class Parser(object):
+class SauerbratenParser(object):
+	_re_name = r'(.*)' # Player/item name
+
+	re_init = re.compile(r'^init: .*$')
+	re_waypoints = re.compile(r'^loaded (\d+) waypoints from ' + _re_name + r'$')
+	re_intermission = re.compile(r'^intermission:$')
+	re_info = re.compile(r'^Info: .*$')
+	re_rendering = re.compile(r'^Rendering using .*$')
+
+	re_game_mode = re.compile(r'^game mode is ' + _re_name + r'$')
+	re_game_ended = re.compile(r'^game has ended!$')
+
+	re_you_disconn = re.compile(r'^disconnected$')
+	re_player_disconn = re.compile(r'^player ' + _re_name + r' disconnected$')
+	re_conn_attempt = re.compile(r'^attempting to connect.*$')
+	re_connected = re.compile(r'^connected to server$')
+	re_player_connected = re.compile(r'^connected: ' + _re_name + r'$')
+	re_connected_from = re.compile(r'^' + _re_name + r' connected from ' +\
+				       _re_name + r'$')
+	re_connected_from2 = re.compile(r'^' + _re_name + r' is fragging in ' +\
+					_re_name + r'$')
+	re_disconn_attempt = re.compile(r'^attempting to disconnect\.\.\.$')
+	re_rename = re.compile(r'^' + _re_name + r' is now known as ' + _re_name + r'$')
+
+	re_read_map = re.compile(r'^read map (.*) \(\d+\.\d+ seconds\)$')
+	re_suggest = re.compile(r'^' + _re_name + r' suggests ' + _re_name + r' on map ' +
+				_re_name + r' \(select map to vote\)$')
+
+	re_you_got_fragged = re.compile(r'^you got fragged by ' + _re_name + r'$')
+	re_fragged = re.compile(r'^' + _re_name + r' fragged ' + _re_name + r'$')
+	re_fragged_teammate = re.compile(r'^' + _re_name + r' fragged a teammate \(' +\
+					 _re_name + r'\)$')
+	re_yougotkilledby_teammate = re.compile(r'^you got fragged by a teammate \(' +\
+						_re_name + r'\)$')
+	re_suicide = re.compile(r'^' + _re_name + r' suicided!?$')
+
+	re_dropped_your = re.compile(r'^' + _re_name + r' dropped your flag$')
+	re_dropped_enemy = re.compile(r'^' + _re_name + r' dropped the enemy flag$')
+	re_stole_your = re.compile(r'^' + _re_name + r' stole your flag$')
+	re_stole_enemy = re.compile(r'^' + _re_name + r' stole the enemy flag$')
+	re_picked_your = re.compile(r'^' + _re_name + r' picked up your flag$')
+	re_picked_enemy = re.compile(r'^' + _re_name + r' picked up the enemy flag$')
+	re_score_your = re.compile(r'^' + _re_name + r' scored for your team$')
+	re_score_enemy = re.compile(r'^' + _re_name + r' scored for the enemy team$')
+	re_returned_your = re.compile(r'^' + _re_name + r' returned your flag$')
+	re_returned_enemy = re.compile(r'^' + _re_name + r' returned the enemy flag$')
+	re_stopped_by = re.compile(r'^' + _re_name + r' was stopped by ' + _re_name + r'$')
+
+	re_rampage = re.compile(r'^' + _re_name + r' is on a RAMPAGE!!$')
+	re_dominate = re.compile(r'^' + _re_name + r' is DOMINATING!!$')
+	re_triplekill = re.compile(r'^' + _re_name + r' scored a TRIPLE KILL!$')
+
+	re_msg = re.compile(r'^' + _re_name + r': .*$')
+	# chat annotation patch
+	re_chat = re.compile(r'^chat-message: ' + _re_name + r':\s*(.*)$')
+	re_teamchat = re.compile(r'^teamchat-message: ' + _re_name + r':\s*(.*)$')
+
 	def __init__(self, options):
 		self.options = options
 		self.currentGame = None
@@ -413,19 +417,19 @@ class Parser(object):
 
 	def parseLine(self, line):
 		line, stamp = self.__parseStamp(line)
-		m = re_chat.match(line)
+		m = self.re_chat.match(line)
 		if m:
 			debugMsg("Chat (%s: %s)" % (m.group(1), m.group(2)))
 			return
-		m = re_teamchat.match(line)
+		m = self.re_teamchat.match(line)
 		if m:
 			debugMsg("Teamchat (%s: %s)" % (m.group(1), m.group(2)))
 			return
-		m = re_info.match(line)
+		m = self.re_info.match(line)
 		if m:
 			debugMsg("Info (%s)" % line)
 			return
-		m = re_game_ended.match(line)
+		m = self.re_game_ended.match(line)
 		if m:
 			if not self.__assertCurrentGame("game ended"):
 				return
@@ -436,7 +440,7 @@ class Parser(object):
 			self.currentGame.ended = stamp
 			self.__gameEnded()
 			return
-		m = re_game_mode.match(line)
+		m = self.re_game_mode.match(line)
 		if m:
 			self.currentGame = None
 			debugMsg("Game mode (%s)" % line)
@@ -446,27 +450,27 @@ class Parser(object):
 			self.currentGame = newGame
 			self.games.append(newGame)
 			return
-		m = re_yougotkilledby_teammate.match(line)
+		m = self.re_yougotkilledby_teammate.match(line)
 		if m:
 			debugMsg("TK 1 (%s)" % line)
 			self.__parseKill(stamp, m.group(1), None, tk=True)
 			return
-		m = re_fragged_teammate.match(line)
+		m = self.re_fragged_teammate.match(line)
 		if m:
 			debugMsg("TK 2 (%s)" % line)
 			self.__parseKill(stamp, m.group(1), m.group(2), tk=True)
 			return
-		m = re_you_got_fragged.match(line)
+		m = self.re_you_got_fragged.match(line)
 		if m:
 			debugMsg("Frag 1 (%s)" % line)
 			self.__parseKill(stamp, m.group(1), None)
 			return
-		m = re_fragged.match(line)
+		m = self.re_fragged.match(line)
 		if m:
 			debugMsg("Frag 2 (%s)" % line)
 			self.__parseKill(stamp, m.group(1), m.group(2))
 			return
-		m = re_suicide.match(line)
+		m = self.re_suicide.match(line)
 		if m:
 			debugMsg("Suicide (%s)" % line)
 			if not self.__assertCurrentGame("suicide"):
@@ -474,7 +478,7 @@ class Parser(object):
 			player = self.currentGame.playerOrMe(m.group(1))
 			player.addSuicide(stamp)
 			return
-		m = re_dropped_your.match(line)
+		m = self.re_dropped_your.match(line)
 		if m:
 			debugMsg("Dropped your flag (%s)" % line)
 			if not self.__assertCurrentGame("dropped-your"):
@@ -482,7 +486,7 @@ class Parser(object):
 			player = self.currentGame.player(m.group(1))
 			player.addCtfDrop(stamp)
 			return
-		m = re_dropped_enemy.match(line)
+		m = self.re_dropped_enemy.match(line)
 		if m:
 			debugMsg("Dropped enemy flag (%s)" % line)
 			if not self.__assertCurrentGame("dropped-enemy"):
@@ -490,7 +494,7 @@ class Parser(object):
 			player = self.currentGame.playerOrMe(m.group(1))
 			player.addCtfDrop(stamp)
 			return
-		m = re_stole_your.match(line)
+		m = self.re_stole_your.match(line)
 		if m:
 			debugMsg("Stole your flag (%s)" % line)
 			if not self.__assertCurrentGame("stole-your"):
@@ -498,7 +502,7 @@ class Parser(object):
 			player = self.currentGame.player(m.group(1))
 			player.addCtfSteal(stamp)
 			return
-		m = re_stole_enemy.match(line)
+		m = self.re_stole_enemy.match(line)
 		if m:
 			debugMsg("Stole enemy flag (%s)" % line)
 			if not self.__assertCurrentGame("stole-enemy"):
@@ -506,7 +510,7 @@ class Parser(object):
 			player = self.currentGame.playerOrMe(m.group(1))
 			player.addCtfSteal(stamp)
 			return
-		m = re_picked_your.match(line)
+		m = self.re_picked_your.match(line)
 		if m:
 			debugMsg("Picked your flag (%s)" % line)
 			if not self.__assertCurrentGame("picked-your"):
@@ -514,7 +518,7 @@ class Parser(object):
 			player = self.currentGame.player(m.group(1))
 			player.addCtfPick(stamp)
 			return
-		m = re_picked_enemy.match(line)
+		m = self.re_picked_enemy.match(line)
 		if m:
 			debugMsg("Picked enemy flag (%s)" % line)
 			if not self.__assertCurrentGame("picked-enemy"):
@@ -522,7 +526,7 @@ class Parser(object):
 			player = self.currentGame.playerOrMe(m.group(1))
 			player.addCtfPick(stamp)
 			return
-		m = re_score_your.match(line)
+		m = self.re_score_your.match(line)
 		if m:
 			debugMsg("Scored for your team (%s)" % line)
 			if not self.__assertCurrentGame("scored-your"):
@@ -530,7 +534,7 @@ class Parser(object):
 			player = self.currentGame.playerOrMe(m.group(1))
 			player.addCtfScore(stamp)
 			return
-		m = re_score_enemy.match(line)
+		m = self.re_score_enemy.match(line)
 		if m:
 			debugMsg("Scored for enemy team (%s)" % line)
 			if not self.__assertCurrentGame("scored-enemy"):
@@ -538,7 +542,7 @@ class Parser(object):
 			player = self.currentGame.player(m.group(1))
 			player.addCtfScore(stamp)
 			return
-		m = re_returned_your.match(line)
+		m = self.re_returned_your.match(line)
 		if m:
 			debugMsg("Returned your flag (%s)" % line)
 			if not self.__assertCurrentGame("returned-your"):
@@ -546,7 +550,7 @@ class Parser(object):
 			player = self.currentGame.playerOrMe(m.group(1))
 			player.addCtfReturn(stamp)
 			return
-		m = re_returned_enemy.match(line)
+		m = self.re_returned_enemy.match(line)
 		if m:
 			debugMsg("Returned enemy flag (%s)" % line)
 			if not self.__assertCurrentGame("returned-enemy"):
@@ -554,27 +558,27 @@ class Parser(object):
 			player = self.currentGame.player(m.group(1))
 			player.addCtfReturn(stamp)
 			return
-		m = re_stopped_by.match(line)
+		m = self.re_stopped_by.match(line)
 		if m:
 			debugMsg("Stopped by (%s)" % line)
 			return
-		m = re_rampage.match(line)
+		m = self.re_rampage.match(line)
 		if m:
 			debugMsg("Rampage (%s)" % line)
 			return
-		m = re_dominate.match(line)
+		m = self.re_dominate.match(line)
 		if m:
 			debugMsg("Dominate (%s)" % line)
 			return
-		m = re_triplekill.match(line)
+		m = self.re_triplekill.match(line)
 		if m:
 			debugMsg("Triplekill (%s)" % line)
 			return
-		m = re_suggest.match(line)
+		m = self.re_suggest.match(line)
 		if m:
 			debugMsg("Suggest (%s)" % line)
 			return
-		m = re_player_connected.match(line)
+		m = self.re_player_connected.match(line)
 		if m:
 			debugMsg("Player connected (%s)" % line)
 			if not self.__assertCurrentGame("player connected"):
@@ -582,7 +586,7 @@ class Parser(object):
 			name = m.group(1)
 			self.currentGame.player(name).connected()
 			return
-		m = re_player_disconn.match(line)
+		m = self.re_player_disconn.match(line)
 		if m:
 			debugMsg("Player disconnected (%s)" % line)
 			if not self.__assertCurrentGame("player connected"):
@@ -590,31 +594,31 @@ class Parser(object):
 			name = m.group(1)
 			self.currentGame.player(name).disconnected()
 			return
-		m = re_conn_attempt.match(line)
+		m = self.re_conn_attempt.match(line)
 		if m:
 			debugMsg("Connection attempt (%s)" % line)
 			return
-		m = re_connected.match(line)
+		m = self.re_connected.match(line)
 		if m:
 			debugMsg("Connected (%s)" % line)
 			return
-		m = re_connected_from.match(line)
+		m = self.re_connected_from.match(line)
 		if m:
 			debugMsg("Connected from (%s)" % line)
 			return
-		m = re_connected_from2.match(line)
+		m = self.re_connected_from2.match(line)
 		if m:
 			debugMsg("Connected from (%s)" % line)
 			return
-		m = re_disconn_attempt.match(line)
+		m = self.re_disconn_attempt.match(line)
 		if m:
 			debugMsg("Disconnect attempt (%s)" % line)
 			return
-		m = re_you_disconn.match(line)
+		m = self.re_you_disconn.match(line)
 		if m:
 			debugMsg("Disconnected (%s)" % line)
 			return
-		m = re_rename.match(line)
+		m = self.re_rename.match(line)
 		if m:
 			debugMsg("Player rename (%s)" % line)
 			if not self.__assertCurrentGame("player rename"):
@@ -623,7 +627,7 @@ class Parser(object):
 			newName = m.group(2)
 			self.currentGame.player(oldName).rename(newName)
 			return
-		m = re_read_map.match(line)
+		m = self.re_read_map.match(line)
 		if m:
 			if self.options.splitLogs:
 				closeRawLog()
@@ -634,23 +638,23 @@ class Parser(object):
 				mapname = mapname[0:-4]
 			self.lastReadMap = mapname
 			return
-		m = re_rendering.match(line)
+		m = self.re_rendering.match(line)
 		if m:
 			debugMsg("Renderer (%s)" % line)
 			return
-		m = re_init.match(line)
+		m = self.re_init.match(line)
 		if m:
 			debugMsg("Init (%s)" % line)
 			return
-		m = re_waypoints.match(line)
+		m = self.re_waypoints.match(line)
 		if m:
 			debugMsg("Waypoints (%s)" % line)
 			return
-		m = re_intermission.match(line)
+		m = self.re_intermission.match(line)
 		if m:
 			debugMsg("Intermission (%s)" % line)
 			return
-		m = re_msg.match(line)
+		m = self.re_msg.match(line)
 		if m:
 			debugMsg("Message (%s)" % line)
 			return
@@ -798,7 +802,7 @@ def lineHasTimestamp(line):
 	return False
 
 def readInput(fd, options):
-	p = Parser(options)
+	p = SauerbratenParser(options)
 	while True:
 		line = fd.readline()
 		if not line:
