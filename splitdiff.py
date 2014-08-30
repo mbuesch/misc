bf8d885026b245f4926c82f0af368db9c43a0e1a
Michael Buesch <m@bues.ch>
2014-08-30 10:14:35 +0200

    splitdiff: Fix handling of headers
    Signed-off-by: Michael Buesch <m@bues.ch>

--- splitdiff.py
+++ splitdiff.py
@@ -5,7 +5,8 @@ import getopt
 
 
 prefix = 1
-inHeader = True
+inPreamble = True
+inPatch = False
 lineNr = 0
 fileNr = 0
 fd = None
@@ -37,37 +38,46 @@ while 1:
 	if not line:
 		break
 	lineNr += 1
-	if line.startswith("diff "):
-		path = line.strip().split(" ")[-1]
-		path = path.split("/")
-		if len(path) <= prefix:
-			die("Could not strip all prefix")
-		path = path[prefix:]
-		filename = "-".join(path)
-		filename = "%03d-%s.diff" % (fileNr, filename)
-		fileNr += 1
-		fd = file(filename, "w")
-		out(line)
-		inHeader = False
+	if line.startswith("diff ") or \
+	   line.startswith("--- ") or \
+	   line.startswith("+++ "):
+		if inPreamble or inPatch:
+			path = line.strip().split(" ")[-1]
+			path = path.split("/")
+			if len(path) <= prefix:
+				die("Could not strip all prefix")
+			path = path[prefix:]
+			filename = "-".join(path)
+			filename = "%03d-%s.diff" % (fileNr, filename)
+			fileNr += 1
+			if fd:
+				fd.close()
+			fd = file(filename, "w")
+			out(line)
+			inPreamble = False
+			inPatch = False
+			continue
+		else:
+			out(line)
+			continue
+	if line.startswith("index "):
+		if not inPreamble:
+			out(line)
 		continue
-	if line.startswith("--- ") or \
-	   line.startswith("+++ ") or \
-	   line.startswith("@@ ") or \
-	   line.startswith("+") or \
-	   line.startswith("-") or \
-	   line.startswith(" ") or \
-	   line.startswith("\\ No newline at end of file") or \
-	   line.startswith("index ") or \
+	if line.startswith("\\ No newline at end of file") or \
 	   line.startswith("old mode") or \
 	   line.startswith("new mode") or \
 	   line.startswith("new file mode ") or \
-	   line.startswith("deleted file mode "):
-		if not inHeader:
+	   line.startswith("deleted file mode ") or \
+	   line.startswith("+") or \
+	   line.startswith("-") or \
+	   line.startswith(" ") or \
+	   line.startswith("@@ "):
+		if not inPreamble:
+			inPatch = True
 			out(line)
 		continue
-
-	if inHeader:
-		continue
-	die("Parse error in line %d" % lineNr)
+	if not inPreamble:
+		die("Parse error in line %d" % lineNr)
 
 sys.exit(0)
