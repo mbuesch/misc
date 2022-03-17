ad319739cdce68561d095c77a4d04f31c20a2234
Michael Buesch <m@bues.ch>
2022-03-17 19:51:00 +0100

    calclambda: 2to3

--- calclambda.py
+++ calclambda.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
-
 
 try:
 	factor = 0.97
@@ -9,8 +8,8 @@ try:
 	upperMhz = float(sys.argv[2])
 	if len(sys.argv) >= 4:
 		factor = float(sys.argv[3])
-except (IndexError, ValueError), e:
-	print "Usage: %s LOWERMHZ UPPERMHZ [FACTOR]" % sys.argv[0]
+except (IndexError, ValueError) as e:
+	print("Usage: %s LOWERMHZ UPPERMHZ [FACTOR]" % sys.argv[0])
 	sys.exit(1)
 
 centerMhz = (upperMhz - lowerMhz) / 2 + lowerMhz
@@ -21,5 +20,5 @@ lambd = (float(c) / centerHz) * factor
 lambd4 = lambd / 4
 lambd4mm = lambd4 * 1000
 
-print "%.1f < %.1f < %.1f  (* %.2f)   ==>   lambda/4 = %.1f mm" %\
-	(lowerMhz, centerMhz, upperMhz, factor, lambd4mm)
+print("%.1f < %.1f < %.1f  (* %.2f)   ==>   lambda/4 = %.1f mm" %\
+	(lowerMhz, centerMhz, upperMhz, factor, lambd4mm))
