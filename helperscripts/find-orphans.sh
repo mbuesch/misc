2f8023f213b3926ec4dd41e415b07a943f92c863
Michael Buesch <m@bues.ch>
2024-12-15 09:45:20 +0100

    find-orphans: Check if lists file does actually exist

--- helperscripts/find-orphans.sh
+++ helperscripts/find-orphans.sh
@@ -3,9 +3,15 @@
 for pkg in $(dpkg --get-selections |\
 	     grep -Ee '[[:space:]]install$' |\
 	     grep -Eoe '^[^[:space:]]+'); do
-	if ! apt-cache showpkg "$pkg" |\
-	     grep -q '/var/lib/apt/lists/'; then
-	   	# Found an orphan. Print its name.
-		echo "$pkg"
+
+	line="$(apt-cache showpkg "$pkg" | grep '/var/lib/apt/lists/' | head -n1)"
+
+	if [ -n "$line" ]; then
+		file="$(echo "$line" | cut -d'(' -f2 | cut -d')' -f1)"
+		if [ -r "$file" ]; then
+			continue
+		fi
 	fi
+	# Found an orphan. Print its name.
+	echo "$pkg"
 done
